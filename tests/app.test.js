7df2b0c0f2575ca13b0fd3f9fcaac16c8c55aa75
Amirhossein Rahmati <you@example.com>
2025-09-16 15:54:17 -0300

    Convert project to CommonJS for Jest

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,10 +1,9 @@
-// tests/app.test.js
-const request = require('supertest');
-const app = require('../src/app');
+const request = require("supertest");
+const app = require("../src/index");
 
-describe('Todo API', () => {
-  it('should return 200 on GET /', async () => {
-    const res = await request(app).get('/');
+describe("Todo API", () => {
+  it("should return 200 on GET /", async () => {
+    const res = await request(app).get("/");
     expect(res.statusCode).toBe(200);
   });
 });
